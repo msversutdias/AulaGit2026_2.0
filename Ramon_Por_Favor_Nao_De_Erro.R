f488f0e2c95efc2a2993a5893e61d33abd1d3968
galapahuz <135077049+galapahuz@users.noreply.github.com>
2026-06-17 15:45:35 -0300

    Comemorei que funcionou

--- Ramon_Por_Favor_Nao_De_Erro.R
+++ Ramon_Por_Favor_Nao_De_Erro.R
@@ -1,9 +1,11 @@
 #Vamos aprender a utilizar o Git integrado ao Rstudio
 
-#Esse script vai funcionar
+#Esse script vai funcionar #Push1
 b<-rnorm(200)
 plot(b)
 
 c<-rnorm(150)
 plot(c)
 
+#Como o teste anterior funcionou, fiquei feliz
+print("Oba funcionou")
